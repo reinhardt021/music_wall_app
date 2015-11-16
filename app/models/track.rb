0d2eef3fba5db37c989c5e0bcb6f11d50da729df
reinhardtcgr <reinhardt.cgr@gmail.com>
2015-11-16 21:07:51 +0000

    Added url validation

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,5 +1,17 @@
 class Track < ActiveRecord::Base
   validates :title, presence: true
   validates :author, presence: true
-  
+  validate :url_valid
+
+  private
+
+  def url_valid
+    unless url.strip.empty?
+      errors.add(:url, "must be valid") unless match_regex?
+    end
+  end 
+
+  def match_regex?
+    url.match(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/)
+  end
 end
