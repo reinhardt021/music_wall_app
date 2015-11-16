163eb495f2ec44ea6fc946645e058abcd0b68822
reinhardtcgr <reinhardt.cgr@gmail.com>
2015-11-16 22:12:57 +0000

    Updated validation for only youtube links

--- app/models/track.rb
+++ app/models/track.rb
@@ -7,12 +7,13 @@ class Track < ActiveRecord::Base
 
   def url_valid
     unless url.strip.empty?
-      errors.add(:url, "must be valid") unless match_regex?
+      errors.add(:url, "must be valid youtube link") unless match_regex?
     end
   end 
 
   def match_regex?
     # url.match(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/)
-    url.match(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})/)
+    # url.match(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})/)
+    url.match(/^https:\/\/www\.youtube\.com\/watch\?..\w+$/)
   end
 end
