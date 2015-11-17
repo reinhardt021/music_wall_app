137566eaf345db96fc7c5efccb40f77dfaf9311e
reinhardtcgr <reinhardt.cgr@gmail.com>
2015-11-17 03:22:59 +0000

    added in the login feature

--- app/actions.rb
+++ app/actions.rb
@@ -31,3 +31,26 @@ get '/tracks/:id' do
   @track = Track.find params[:id]
   erb :'/tracks/show'
 end
+
+post '/votes' do
+  vote = Vote.create(track_id: params[:track_id],user_id: params[:user_id])
+  if vote.persisted?
+    redirect '/tracks'
+  else
+    redirect '/tracks?message=Cannot vote for song twice'
+  end
+end
+
+post '/login' do
+  user = User.find_by(email: params[:email])
+  if user
+    if user.password == params[:password]
+      session[:user_id] = user.id
+      redirect '/tracks'
+    else
+      redirect '/login?message=Invalid password'  
+    end
+  else
+    redirect '/login?message=Invalid email'
+  end
+end
