15a0c5ef988340cfa10391f360dd76cef8864e65
reinhardtcgr <reinhardt.cgr@gmail.com>
2015-11-17 20:15:28 +0000

    Added sign up feature

--- app/actions.rb
+++ app/actions.rb
@@ -1,9 +1,10 @@
 # Homepage (Root path)
 get '/' do
+  @user = User.new
   erb :index
 end
 
-
+# Note that each of these are independent of each other as HTTP requests
 get '/tracks' do
   @tracks = Track.all
   erb :'tracks/index'
@@ -18,7 +19,8 @@ post '/tracks' do
   @track = Track.new(
     title:  params[:title],
     author: params[:author], 
-    url:    params[:url]
+    url:    params[:url],
+    user_id: params[:user_id]
   )
   if @track.save
     redirect '/tracks'
@@ -55,6 +57,7 @@ post '/login' do
   if user
     if user.password == params[:password]
       session[:user_id] = user.id
+      session[:user_email] = user.email
       redirect '/tracks'
     else
       redirect '/login?message=Invalid password'  
@@ -67,6 +70,21 @@ end
 post '/logout' do
   session.clear
   session[:user_id] = nil
-  redirect '/tracks'
+  redirect '/'
+end
+
+post '/signup' do
+  @user = User.new(
+    email: params[:email],
+    password: params[:password]
+  )
+  if @user.save
+    session[:user_id] = @user.id
+    session[:user_email] = @user.email
+    redirect '/tracks'
+  else
+    erb :'index'
+  end
+
 end
 
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,5 +1,4 @@
 class Track < ActiveRecord::Base
-
   belongs_to :user
   has_many :votes
 
