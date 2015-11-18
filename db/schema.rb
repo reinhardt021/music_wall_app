2b029ab668df55fb6dc3a7b232d6344b4aaccb81
reinhardtcgr <reinhardt.cgr@gmail.com>
2015-11-18 08:02:53 +0000

    Added rating field to reviews table and created form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118033315) do
+ActiveRecord::Schema.define(version: 20151118080123) do
 
   create_table "reviews", force: :cascade do |t|
     t.string   "comment"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20151118033315) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "rating"
   end
 
   create_table "tracks", force: :cascade do |t|
